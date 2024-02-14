c9f67aff2698ed945344d6542452ff92096b6cc6
Zatomate <duthoit.thomas9@gmail.com>
2024-02-14 15:36:53 +0100

    premier build fonctionnel

--- standalone/standalone.c
+++ standalone/standalone.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <avalam.h>
+#include <topologie.h>
 
-int main() {
+typedef struct  //definie la structure d'un joueur
+{
+    T_Score score;  // définie le score du joueur 
+    char pseudo[20];  //pseudo de 20 caractères max
+} T_joueur;
+
+
+int main() {  // Point d'entré du programme
+
+    T_joueur joueur1;
+    T_joueur joueur2;
+
+    T_Position p = getPositionInitiale();  // On récupére la position initiale du plateau
+    T_ListeCoups l = getCoupsLegaux(p);
+	printf("Depuis la position initiale du jeu, il y a %d coups possibles\n", l.nb);
+
+	printf("Depuis la position initiale du jeu, le trait est aux %ss\n", COLNAME(p.trait));
     return 0;
 }
