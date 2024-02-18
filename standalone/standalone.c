a2cc7dc442efb337539cd1e3be740667332d079b
Zatomate <duthoit.thomas9@gmail.com>
2024-02-18 12:08:15 +0100

    fin de partie implémentée + stupidobot

--- standalone/standalone.c
+++ standalone/standalone.c
@@ -13,7 +13,7 @@ typedef struct  //definie la structure d'un joueur
 } T_joueur;
 
 int coup(T_Position *p);
-int coupV2(T_Position *p);
+void coupV2(T_Position *p);
 
 
 int main() {  // Point d'entré du programme
@@ -24,6 +24,15 @@ int main() {  // Point d'entré du programme
     T_Position p = getPositionInitiale();  // On récupére la position initiale du plateau
     T_ListeCoups l = getCoupsLegaux(p);
     T_Score s = {0, 0, 0, 0};  // Par défaut, les scores sont de 0 pour chaque joueurs
+
+
+    // TODO: retirer après les tests
+    T_Coup coup_auto = l.coups[0];
+
+
+    int joue = 1;  // booléen pour savoir si on joue ou non
+
+    printf("----- A V A L A M   P A R   L A   T E A M   A V A L P I E R R E -----\n\n");
 
 
     printf("Nom Joueur 1: %s   -   Trait: %s\n", joueur1.pseudo, COLNAME(joueur1.trait));
@@ -37,12 +46,66 @@ int main() {  // Point d'entré du programme
     // printf("\nListe des coups possibles:\n");
     // afficherListeCoups(l);
     // coup(&p);
-    printf("Position actuelle:\n");
-    afficherPosition(p);
-    coupV2(&p);
-    // afficherPosition(p);
+    
+    while (joue)
+    {
+        /*
+        // -------------------------- //
+        // MODE MANUEL
+        printf("Position actuelle:\n");
+        afficherPosition(p);  // affichage de l'état actuel du plateau
+        coupV2(&p);  // Saisie du coup pour l'utilisateur
+        // -------------------------- //
+        */
+        // -------------------------- //
+        // MODE AUTOMATIQUE (on joue le premier coup légal automatiquement) aka STUPIDO_BOT
+        printf("Coups légaux: %d\n", l.nb);
+        coup_auto = l.coups[0];
+        p = jouerCoup(p, coup_auto.origine, coup_auto.destination);
+        // -------------------------- //
+        s = evaluerScore(p);
+        printf("Score actuel: ");
+        afficherScore(s);
+        l = getCoupsLegaux(p);
+        if (l.nb == 0) {
+            // Plus de coups légaux à jouer -> fin de la partie
+            joue = 0;
+        }
+    }
+    printf("\n\n----- F I N   D E   L A   P A R T I E -----\n\n");
+    printf("Score final: ") ;
+    afficherScore(s);
+    if (s.nbJ > s.nbR) printf("%s a gagné !\n", joueur1.pseudo);
+    else if (s.nbJ < s.nbR) printf("%s a gagné !\n", joueur2.pseudo);
+    else if (s.nbJ == s.nbR) {
+        if (s.nbJ5 > s.nbR5) printf("%s a gagné !\n", joueur1.pseudo);
+        else if (s.nbJ5 < s.nbR5) printf("%s a gagné !\n", joueur2.pseudo);
+        else printf("Egalité entre les deux joueurs !\n");
+    }
     return 0;
 }
+
+
+void coupV2(T_Position *p) {
+    int depart, fin;
+    printf("Quel pile souhaitez vous utiliser ? [-1 pour afficher tous les coups légaux possibles] ");
+    scanf("%d", &depart);
+    if (depart == -1) {
+        printf("Coups légaux:\n");
+        afficherListeCoups(getCoupsLegaux(*p));
+        printf("Quel pile souhaitez vous utiliser ? ");
+        scanf("%d", &depart);
+    } else {
+        printf("Voisins de la case '%d': ", depart);
+        listerVoisins(depart);
+    }
+    
+    printf("Où souhaitez vous la poser ? ");
+    scanf("%d", &fin);  
+    if (estValide(*p, depart, fin)) *p = jouerCoup(*p, depart, fin);
+    else coupV2(p);
+}
+
 
 int coup(T_Position *p){
     T_ListeCoups listecoup = getCoupsLegaux(*p);
@@ -56,20 +119,4 @@ int coup(T_Position *p){
         return 1;
     }
     else return coup(p);
-}
-
-int coupV2(T_Position *p) {
-    int depart, fin;
-    printf("Quel pile souhaitez vous utiliser ? ");
-    scanf("%d", &depart);
-    printf("Voisins de la case '%d': ", depart);
-    listerVoisins(depart);
-    printf("Où souhaitez vous la poser ? ");
-    scanf("%d", &fin);  
-    if (estValide(*p, depart, fin)) {
-        *p = jouerCoup(*p, depart, fin);
-        afficherPosition(*p);
-        return 1;
-    }
-    else return coupV2(p);
 }
