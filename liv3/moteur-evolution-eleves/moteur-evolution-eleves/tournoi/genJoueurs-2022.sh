6ae0cace5f702a7ebc430bdefa29413f2e808fd7
Blackun1 <146175655+Blackun1@users.noreply.github.com>
2024-05-13 19:15:04 +0200

    Fix moteur

---
+++ liv3/moteur-evolution-eleves/moteur-evolution-eleves/tournoi/genJoueurs-2022.sh
@@ -0,0 +1,36 @@
+#! /bin/bash 
+
+./joueurs-2022/2022_aarise.exe aarise &
+sleep 1
+./joueurs-2022/2022_abtal.exe abtal &
+sleep 1
+./joueurs-2022/2022_abtalV2.exe abtalV2 &
+sleep 1
+./joueurs-2022/2022_atom.exe atom &
+sleep 1
+./joueurs-2022/2022_avalens.exe avalens &
+sleep 1
+./joueurs-2022/2022_ekip.exe ekip &
+sleep 1
+./joueurs-2022/2022_evalam.exe evalam &
+sleep 1
+./joueurs-2022/2022_javak.exe javak &
+sleep 1
+./joueurs-2022/2022_javakV2.exe javakV2 &
+sleep 1
+./joueurs-2022/2022_joyAvalam.exe joyAvalam &
+sleep 1
+./joueurs-2022/2022_kyra.exe kyra &
+sleep 1
+./joueurs-2022/2022_lana.exe lana &
+sleep 1
+./joueurs-2022/2022_lanaV2.exe lanaV2 &
+sleep 1
+./joueurs-2022/2022_makabot.exe makabot &
+sleep 1
+./joueurs-2022/2022_ranval.exe ranval &
+sleep 1
+./joueurs-2022/2022_rhoades.exe rhoades &
+sleep 1
+./joueurs-2022/2022_suuu.exe suuu &
+sleep 1
---
+++ liv3/moteur-evolution-eleves/moteur-evolution-eleves/tournoi/genJoueurs-2023.sh
@@ -0,0 +1,35 @@
+#! /bin/bash 
+
+./joueurs-2023/2023_atchoum.exe atchoum &
+sleep 1
+./joueurs-2023/2023_avalacadabra.exe avalacadabra &
+sleep 1
+./joueurs-2023/2023_avalajeanperrin.exe avalajeanperrin & 
+sleep 1
+./joueurs-2023/2023_avalamentable.exe avalamentable &
+sleep 1
+./joueurs-2023/2023_avalamoi.exe avalamoi & 
+sleep 1
+./joueurs-2023/2023_avalattaque.exe avalattaque &
+sleep 1
+./joueurs-2023/2023_avalbytes.exe avalbytes &
+sleep 1
+./joueurs-2023/2023_avalenciennes.exe avalenciennes &
+sleep 1
+./joueurs-2023/2023_avaletout.exe avaletout & 
+sleep 1
+./joueurs-2023/2023_avaleurs.exe avaleurs &
+sleep 1
+./joueurs-2023/2023_avalfish.exe avalfish &
+sleep 1
+./joueurs-2023/2023_avalla.exe avalla &
+sleep 1
+./joueurs-2023/2023_avalum.exe avalum &
+sleep 1
+./joueurs-2023/2023_avatar.exe avatar &
+sleep 1
+./joueurs-2023/2023_malmaisvole.exe malmaisvole &
+sleep 1
+./joueurs-2023/2023_null.exe null & 
+sleep 1
+
