72883f777c7698d60b0fa42a104d9709062caf29
Blackun1 <146175655+Blackun1@users.noreply.github.com>
2024-05-15 15:14:18 +0200

    Update joueur6.c

--- liv3/moteur-evolution-eleves/joueurs/joueur6.c
+++ liv3/moteur-evolution-eleves/joueurs/joueur6.c
@@ -10,13 +10,55 @@
 
 
 void choisirCoup(T_Position currentPosition, T_ListeCoups listeCoups) {
-	char i; // Astuce de Mr. Oxoby: un char est plus rapide car en incrémentant, on fait les calculs sur 1 octet et pas 4
+	int i, j, check = 0;
 	octet o, d; 
 	octet myColor = currentPosition.trait; 
-    printf("démarrage du bot !\n");
-    getchar();
-    printf("TEST MODULE: %s", TEST);
-    printf("COLOR: %d\n", myColor);
-    ecrireIndexCoup(0);
+    T_Voisins voisins;
 
+
+	// afficherListeCoups(listeCoups);
+
+	printf("Ma couleur : %s\n", COLNAME(currentPosition.trait));
+	for(i=0;i<listeCoups.nb; i++) {
+		o = listeCoups.coups[i].origine; 
+		d = listeCoups.coups[i].destination;
+
+        if  ((currentPosition.cols[o].couleur == myColor) &&                           // Vérification des couleurs d'origine et destination     
+            (currentPosition.cols[d].couleur != myColor)){
+
+                switch(currentPosition.cols[o].nb + currentPosition.cols[d].nb)
+                {
+
+                case 5:                                                               // Prise de pile pour former une pile de 5
+                    ecrireIndexCoup(i);
+                    break;
+                
+
+                case 3:                                                              // Prise de pile ou de jeton pour former une pile de 3
+                    voisins = getVoisins(d);
+                    j = voisins.nb;
+                    check = 0;
+                    do
+                    {
+                        if((currentPosition.cols[voisins.cases[j]].couleur != myColor) &&       // Vérification du danger (pile de 2 adverse autour de la case de destination)
+                        (currentPosition.cols[voisins.cases[j]].nb == 2))
+                            check += 1; 
+                        j -= 1;
+                    } while (j != 0 && check != 1);
+                    if (check == 0){
+                        ecrireIndexCoup(i);
+                    }
+                    break;
+
+
+                default:                                                             // Utiliser pour limiter la "casse"                                                   
+                    if(currentPosition.cols[o].nb + currentPosition.cols[d].nb != 4)
+                        ecrireIndexCoup(i);
+                    break;
+
+            }
+        }
+            
+    }
+                            
 }
